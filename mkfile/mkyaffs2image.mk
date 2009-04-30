924227ed4423d5a8d05b25f7dfe3c1d032c38986
mask <mask@bf80cc59-7c04-4460-961f-feaea741d6cd>
2009-04-30 07:28:04 +0000

    update the new building system

--- mkfile/mkyaffs2image.mk
+++ mkfile/mkyaffs2image.mk
@@ -1,4 +1,6 @@
 
+MODULES += mkyaffs2image
+
 .PHONY: build_mkyaffs2image install_mkyaffs2image clean_mkyaffs2image
 build_mkyaffs2image:
 	if [ ! -d $(PRJROOT)/$(call path-for,mkyaffs2image) ] ; then \
@@ -7,6 +9,10 @@ build_mkyaffs2image:
 	fi
 	$(MAKE) clean_mkyaffs2image
 	$(MAKE) -C $(PRJROOT)/$(call path-for,mkyaffs2image)
+	if [ ! -x $(PRJROOT)/$(call path-for,mkyaffs2image)/mkyaffs2image ] ; then \
+		echo Generating mkyaffs2image FAIL: $(PRJROOT)/$(call path-for,mkyaffs2image)/mkyaffs2image; \
+		exit 1; \
+	fi
 
 install_mkyaffs2image:
 clean_mkyaffs2image:
