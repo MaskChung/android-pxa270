99934d6390783cb2a7b26cb3c69893c5b2528f5b
mask <mask@bf80cc59-7c04-4460-961f-feaea741d6cd>
2009-04-30 11:30:52 +0000

    update the new building system

--- mkfile/setenv.mk
+++ mkfile/setenv.mk
@@ -1,5 +1,14 @@
-###export PRJROOT:=$(shell pwd)
-#include $(PRJROOT)/.config
 
-#include pathmap.mk
+export ARCH			:= arm
+export CROSS_COMPILE := $(patsubst %gcc,%,$(shell find $(call path-for,toolchain) -maxdepth 3 -name "*gcc" -type f | sed 's/^.*bin\///g'))
+
+export ASM			:= $(CROSS_COMPILE)as
+export LD			:= $(CROSS_COMPILE)ld
+export CC			:= $(CROSS_COMPILE)gcc
+export CPP			:= $(CROSS_COMPILE)c++
+export AR			:= $(CROSS_COMPILE)ar
+export STRIP   			:= $(CROSS_COMPILE)strip
+export OBJCOPY			:= $(CROSS_COMPILE)objcopy
+export OBJDUMP			:= $(CROSS_COMPILE)objdump
+export RANLIB  			:= $(CROSS_COMPILE)ranlib
 
