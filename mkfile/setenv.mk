119f3aa6f89aab76cdd968bbe68291cd6831e1a8
mask <mask@bf80cc59-7c04-4460-961f-feaea741d6cd>
2009-05-02 08:01:18 +0000

    update the new building system

--- mkfile/setenv.mk
+++ mkfile/setenv.mk
@@ -1,14 +1,14 @@
 
 export ARCH			:= arm
-export CROSS_COMPILE := $(patsubst %gcc,%,$(shell find $(call path-for,toolchain) -maxdepth 3 -name "*gcc" -type f | sed 's/^.*bin\///g'))
+export CROSS_COMPILE = $(patsubst %gcc,%,$(shell find $(call path-for,toolchain) -maxdepth 3 -name "*gcc" -type f | sed 's/^.*bin\///g'))
 
-export ASM			:= $(CROSS_COMPILE)as
-export LD			:= $(CROSS_COMPILE)ld
-export CC			:= $(CROSS_COMPILE)gcc
-export CPP			:= $(CROSS_COMPILE)c++
-export AR			:= $(CROSS_COMPILE)ar
-export STRIP   			:= $(CROSS_COMPILE)strip
-export OBJCOPY			:= $(CROSS_COMPILE)objcopy
-export OBJDUMP			:= $(CROSS_COMPILE)objdump
-export RANLIB  			:= $(CROSS_COMPILE)ranlib
+export ASM			= $(CROSS_COMPILE)as
+export LD			= $(CROSS_COMPILE)ld
+export CC			= $(CROSS_COMPILE)gcc
+export CPP			= $(CROSS_COMPILE)c++
+export AR			= $(CROSS_COMPILE)ar
+export STRIP   			= $(CROSS_COMPILE)strip
+export OBJCOPY			= $(CROSS_COMPILE)objcopy
+export OBJDUMP			= $(CROSS_COMPILE)objdump
+export RANLIB  			= $(CROSS_COMPILE)ranlib
 
