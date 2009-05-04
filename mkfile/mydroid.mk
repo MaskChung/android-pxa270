ec14d083a29468c8aaeabe487e41b745e7fba59b
mask <mask.chung@gmail.com>
2009-05-04 04:05:39 +0000

    update the new building system for mydroid

--- mkfile/mydroid.mk
+++ mkfile/mydroid.mk
@@ -1,2 +1,17 @@
+
+MODULES += mydroid
+
+.PHONY: build_mydroid install_mydroid clean_mydroid
 ifdef MYDROID
+MYDROID_SRC := $(patsubst "%",%,$(MYDROID_SRC))
+ifneq "$(MYDROID_SRC)" ""
+export MYDROID_SRC
+build_mydroid:
+	echo $(MYDROID)
+	echo $(MYDROID_SRC)
+	$(MAKE) -C $(PRJROOT)/$(call path-for,mydroid)
 endif
+endif
+
+install_mydroid:
+clean_mydroid:
