26225aefd7631e3a2eb3cca201d1312e7d1c04ee
mask <mask.chung@gmail.com>
2009-05-05 04:59:16 +0000

    update the new building system

--- mkfile/rootfs.mk
+++ mkfile/rootfs.mk
@@ -1,17 +1,3 @@
-#
-##export BASE_ROOTFS		:= $(ROOTFS_DIR)/$(patsubst "%",%,$(BASE_ROOTFS))
-##export ANDROID
-##export ANDROID_ROOTFS		:= $(patsubst "%",%,$(ANDROID_ROOTFS))
-###export TARGET_ANDROID_ROOTFS_DIR	:= $(TARGET_DIR)/android_rootfs
-##export ANDROID_GIT
-##export ANDROID_GIT_ROOTFS	:= $(patsubst "%",%,$(ANDROID_GIT_ROOTFS))
-#
-###export ANDROID
-#export ANDROID_ROOTFS		:= $(patsubst "%",%,$(ANDROID_ROOTFS))
-##export TARGET_ANDROID_ROOTFS_DIR	:= $(TARGET_DIR)/android_rootfs
-#export ANDROID_GIT
-#export ANDROID_GIT_ROOTFS	:= $(patsubst "%",%,$(ANDROID_GIT_ROOTFS))
-
 
 BASE_ROOTFS := $(PRJROOT)/$(call path-for,rootfs)/$(patsubst "%",%,$(BASE_ROOTFS))
 DEMO_ROOTFS := $(PRJROOT)/$(call path-for,rootfs)/demo.tgz
@@ -29,6 +15,7 @@ ifeq "$(ANDROID)" "y"
 	mkdir -p $(PRJROOT)/$(call path-for,target-android-rootfs)
 ifeq "$(ANDROID_DEMO)" "y"
 	file -b $(DEMO_ROOTFS) | awk '{print $$1 " -d -c -v $(DEMO_ROOTFS)"}' | sh - | tar xvf - -C $(PRJROOT)/$(call path-for,target-android-rootfs)
+	chmod -R a+rwx $(PRJROOT)/$(call path-for,target-android-rootfs)
 endif
 endif
 
