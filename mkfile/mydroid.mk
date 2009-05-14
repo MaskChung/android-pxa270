62dcf088f5186d195f90e717b14aea98c3b47116
mask <mask.chung@gmail.com>
2009-05-14 16:21:17 +0000

    almost finish the new building system but not tested completely

--- mkfile/mydroid.mk
+++ mkfile/mydroid.mk
@@ -3,7 +3,7 @@ MODULES += mydroid
 
 ifneq "$(MYDROID_SRC)" ""
 export MYDROID_SRC := $(patsubst "%",%,$(MYDROID_SRC))
-$(PRJROOT)/$(call path-for,mydroid)/$(MYDROID_SRC)/out/target/product/generic
+MYDROID_BIN := $(PRJROOT)/$(call path-for,mydroid)/$(MYDROID_SRC)/out/target/product/generic
 endif
 
 .PHONY: build_mydroid install_mydroid clean_mydroid
@@ -14,9 +14,18 @@ endif
 
 install_mydroid:
 ifneq "$(MYDROID_SRC)" ""
-	$(PRJROOT)/$(call path-for,mydroid)/$(MYDROID_SRC)/out/target/product/generic
-mydroid/cdma-import/out/target/product/generic/
-	$(MAKE) -C $(PRJROOT)/$(call path-for,mydroid) clean
+	-rm -rf $(PRJROOT)/$(call path-for,target-mydroid-rootfs)
+	mkdir -p $(PRJROOT)/$(call path-for,target-mydroid-rootfs)
+ifneq "$(BASE_ROOTFS)" ""
+	file -b $(BASE_ROOTFS) | awk '{print $$1 " -d -c -v $(BASE_ROOTFS)"}' | sh - | tar xvf - --strip-components=1 -C $(PRJROOT)/$(call path-for,target-mydroid-rootfs)
+	rsync -r --exclude='.svn' $(PRJROOT)/$(call path-for,rootfs-overwrite)/* $(PRJROOT)/$(call path-for,target-mydroid-rootfs)
+endif
+	cp -af $(MYDROID_BIN)/system $(PRJROOT)/$(call path-for,target-mydroid-rootfs)
+	cp -af $(MYDROID_BIN)/data $(PRJROOT)/$(call path-for,target-mydroid-rootfs)
+	cp -af $(MYDROID_BIN)/root/* $(PRJROOT)/$(call path-for,target-mydroid-rootfs)
+	cd $(PRJROOT)/$(call path-for,target-mydroid-rootfs)/etc && ln -s ../system/etc/* .
+	rsync -r --exclude='.svn' $(PRJROOT)/$(call path-for,rootfs-overwrite-android)/* $(PRJROOT)/$(call path-for,target-mydroid-rootfs)
+	chmod -R a+rwx $(PRJROOT)/$(call path-for,target-mydroid-rootfs)
 endif
 
 clean_mydroid:
