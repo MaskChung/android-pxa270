b51ade126ad651285d7025874e89ce4966ba332a
mask <mask@bf80cc59-7c04-4460-961f-feaea741d6cd>
2009-05-15 08:43:07 +0000

    finish the touch driver, the touch driver is complete done

--- kernel/linux-2.6.25-android-1.0_r1/drivers/input/touchscreen/ucb1400_ts.c
+++ kernel/linux-2.6.25-android-1.0_r1/drivers/input/touchscreen/ucb1400_ts.c
@@ -77,97 +77,6 @@
 
 #define UCB_ID			0x7e
 #define UCB_ID_1400             0x4304
-/*
-#define UCB_IO_DATA	0x00
-#define UCB_IO_DIR	0x01
-
-#define UCB_IO_0		(1 << 0)
-#define UCB_IO_1		(1 << 1)
-#define UCB_IO_2		(1 << 2)
-#define UCB_IO_3		(1 << 3)
-#define UCB_IO_4		(1 << 4)
-#define UCB_IO_5		(1 << 5)
-#define UCB_IO_6		(1 << 6)
-#define UCB_IO_7		(1 << 7)
-#define UCB_IO_8		(1 << 8)
-#define UCB_IO_9		(1 << 9)
-
-#define UCB_IE_RIS	0x02
-#define UCB_IE_FAL	0x03
-#define UCB_IE_STATUS	0x04
-#define UCB_IE_CLEAR	0x04
-#define UCB_IE_ADC		(1 << 11)
-#define UCB_IE_TSPX		(1 << 12)
-#define UCB_IE_TSMX		(1 << 13)
-#define UCB_IE_TCLIP		(1 << 14)
-#define UCB_IE_ACLIP		(1 << 15)
-
-#define UCB_IRQ_TSPX		12
-
-#define UCB_TC_A	0x05
-#define UCB_TC_A_LOOP		(1 << 7)	// UCB1200 
-#define UCB_TC_A_AMPL		(1 << 7)	// UCB1300 
-
-#define UCB_TC_B	0x06
-#define UCB_TC_B_VOICE_ENA	(1 << 3)
-#define UCB_TC_B_CLIP		(1 << 4)
-#define UCB_TC_B_ATT		(1 << 6)
-#define UCB_TC_B_SIDE_ENA	(1 << 11)
-#define UCB_TC_B_MUTE		(1 << 13)
-#define UCB_TC_B_IN_ENA		(1 << 14)
-#define UCB_TC_B_OUT_ENA	(1 << 15)
-
-#define UCB_AC_A	0x07
-#define UCB_AC_B	0x08
-#define UCB_AC_B_LOOP		(1 << 8)
-#define UCB_AC_B_MUTE		(1 << 13)
-#define UCB_AC_B_IN_ENA		(1 << 14)
-#define UCB_AC_B_OUT_ENA	(1 << 15)
-
-#define UCB_TS_CR	0x09
-#define UCB_TS_CR_TSMX_POW	(1 << 0)
-#define UCB_TS_CR_TSPX_POW	(1 << 1)
-#define UCB_TS_CR_TSMY_POW	(1 << 2)
-#define UCB_TS_CR_TSPY_POW	(1 << 3)
-#define UCB_TS_CR_TSMX_GND	(1 << 4)
-#define UCB_TS_CR_TSPX_GND	(1 << 5)
-#define UCB_TS_CR_TSMY_GND	(1 << 6)
-#define UCB_TS_CR_TSPY_GND	(1 << 7)
-#define UCB_TS_CR_MODE_INT	(0 << 8)
-#define UCB_TS_CR_MODE_PRES	(1 << 8)
-#define UCB_TS_CR_MODE_POS	(2 << 8)
-#define UCB_TS_CR_BIAS_ENA	(1 << 11)
-#define UCB_TS_CR_TSPX_LOW	(1 << 12)
-#define UCB_TS_CR_TSMX_LOW	(1 << 13)
-
-#define UCB_ADC_CR	0x0a
-#define UCB_ADC_SYNC_ENA	(1 << 0)
-#define UCB_ADC_VREFBYP_CON	(1 << 1)
-#define UCB_ADC_INP_TSPX	(0 << 2)
-#define UCB_ADC_INP_TSMX	(1 << 2)
-#define UCB_ADC_INP_TSPY	(2 << 2)
-#define UCB_ADC_INP_TSMY	(3 << 2)
-#define UCB_ADC_INP_AD0		(4 << 2)
-#define UCB_ADC_INP_AD1		(5 << 2)
-#define UCB_ADC_INP_AD2		(6 << 2)
-#define UCB_ADC_INP_AD3		(7 << 2)
-#define UCB_ADC_EXT_REF		(1 << 5)
-#define UCB_ADC_START		(1 << 7)
-#define UCB_ADC_ENA		(1 << 15)
-
-#define UCB_ADC_DATA	0x0b
-#define UCB_ADC_DAT_VALID		(1 << 15)
-#define UCB_ADC_DAT_VALUE(x)		(((x) & 0x7fe0) >> 5)
-
-#define UCB_ID		0x0c
-#define UCB_ID_1200		0x1004
-#define UCB_ID_1300		0x1005
-#define UCB_ID_1400		0x4304
-
-#define UCB_MODE	0x0d
-#define UCB_MODE_DYN_VFLAG_ENA	(1 << 12)
-#define UCB_MODE_AUD_OFF_CAN	(1 << 13)
-*/
 
 
 struct ucb1400 {
@@ -347,47 +256,17 @@ static inline void ucb1400_ts_irq_disable(struct ucb1400 *ucb)
 static void ucb1400_ts_evt_add(struct input_dev *idev, u16 pressure, u16 x, u16 y)
 {
 /* it's hard code here, may be you should modify */
-printk(" ----------- report x = %u\n",x);
-printk(" ----------- report y = %u\n",y);
-printk(" ----------- report pressure = %u\n",pressure);
-/*
-x = x > _max_x ? _max_x-1 : x;
-y = y > _max_y ? _max_y-1 : y;
-x = x < _min_x ? _min_x : x;
-y = y < _min_y ? _min_y : y;
-*/
-if(x > g_max_x)
-	x = g_max_x-1;
-else if(x<g_min_x)
-	x=g_min_x+1;
-if(y > g_max_y)
-	y = g_max_y-1;
-else if(y<g_min_y)
-	y = g_min_y+1;
-	/*
-x -= g_min_x;
-y -= g_min_y;
-*/
-x = ((x-g_min_x) * 239) / (g_max_x - g_min_x);
-y = ((y-g_min_y) * 319) / (g_max_y - g_min_y);
-printk(" ----------- cal x = %u\n",x);
-printk(" ----------- cal y = %u\n",y);
-/*
-if(x>=240)
-	x=239;
-if(y>=320)
-	y=319;
-	*/
-/*
-x = x >= 240 ? 240-1 : x;
-y = y >= 320 ? 320-1 : y;
-*/
-//y = _max_y - y;
-//y = (g_max_y -g_min_y)- y;
-y=320-y;
-//y = (y <= 0) ? 1 : y;
-printk(" ----------- cal x = %u\n",x);
-printk(" ----------- cal y = %u\n",y);
+	if(x > g_max_x)
+		x = g_max_x - 1;
+	else if(x < g_min_x)
+		x = g_min_x + 1;
+	if(y > g_max_y)
+		y = g_max_y - 1;
+	else if(y < g_min_y)
+		y = g_min_y + 1;
+	x = ((x - g_min_x) * 239) / (g_max_x - g_min_x);
+	y = ((y - g_min_y) * 319) / (g_max_y - g_min_y);
+	y = 320 - y;
 	input_report_key(idev, BTN_TOUCH, 1);
 	input_report_abs(idev, ABS_X, x);
 	input_report_abs(idev, ABS_Y, y);
@@ -397,7 +276,6 @@ printk(" ----------- cal y = %u\n",y);
 
 static void ucb1400_ts_event_release(struct input_dev *idev)
 {
-printk(" ----------- into ucb1400_ts_event_release\n");
 	input_report_key(idev, BTN_TOUCH, 0);
 	input_report_abs(idev, ABS_PRESSURE, 0);
 	input_sync(idev);
@@ -426,7 +304,6 @@ static int ucb1400_ts_thread(void *_ucb)
 	int valid = 0;
 	struct sched_param param = { .sched_priority = 1 };
 
-printk("%s:%s ---------- into\n",__FILE__,__func__);
 	sched_setscheduler(tsk, SCHED_FIFO, &param);
 
 	set_freezable();
@@ -434,7 +311,6 @@ printk("%s:%s ---------- into\n",__FILE__,__func__);
 		unsigned int x, y, p;
 		long timeout;
 
-	printk(" haha --------------------\n");
 		ucb->ts_restart = 0;
 
 		if (ucb->irq_pending) {
@@ -498,8 +374,6 @@ printk("%s:%s ---------- into\n",__FILE__,__func__);
 static irqreturn_t ucb1400_hard_irq(int irqnr, void *devid)
 {
 	struct ucb1400 *ucb = devid;
-printk("%s: irqnr = %d\n",__func__,irqnr);
-printk("%s: ucb->irq = %d\n",__func__,ucb->irq);
 
 	if (irqnr == ucb->irq) {
 		disable_irq(ucb->irq);
@@ -515,7 +389,6 @@ static int ucb1400_ts_open(struct input_dev *idev)
 	struct ucb1400 *ucb = input_get_drvdata(idev);
 	int ret = 0;
 
-printk("%s:%s ---------- into\n",__FILE__,__func__);
 	BUG_ON(ucb->ts_task);
 
 	ucb->ts_task = kthread_run(ucb1400_ts_thread, ucb, "UCB1400_ts");
@@ -660,17 +533,13 @@ static int ucb1400_ts_probe(struct device *dev)
 	input_set_drvdata(idev, ucb);
 
 	idev->dev.parent	= dev;
-	//idev->name		= "UCB1400-touchscreen";
 	idev->name		= "UCB1400 touchscreen interface";
 	idev->id.vendor		= ucb1400_reg_read(ucb, AC97_VENDOR_ID1);
 	idev->id.product	= id;
 	idev->open		= ucb1400_ts_open;
 	idev->close		= ucb1400_ts_close;
-//	idev->evbit[0]		= BIT(EV_ABS);
-	//idev->evbit[0]		= BIT_MASK(EV_ABS);
 	set_bit(EV_KEY,idev->evbit);
 	set_bit(EV_ABS,idev->evbit);
-//	set_bit(EV_SYN,idev->evbit);
 	set_bit(BTN_TOUCH,idev->keybit);
 
 	ucb1400_adc_enable(ucb);
@@ -680,20 +549,9 @@ static int ucb1400_ts_probe(struct device *dev)
 	printk("UCB1400: x/y = %d/%d\n", x_res, y_res);
 
 /* it's hard code here, may be you should modify */
-/*
 	input_set_abs_params(idev, ABS_X, 0, 240, 0, 0);
 	input_set_abs_params(idev, ABS_Y, 0, 320, 0, 0);
-	*/
-//	input_set_abs_params(idev, ABS_X, 0, g_max_x-g_min_x, 6, 0);
-//	input_set_abs_params(idev, ABS_Y, 0, g_max_y-g_min_y, 6, 0);
-	input_set_abs_params(idev, ABS_X, 0, 240, 6, 0);
-	input_set_abs_params(idev, ABS_Y, 0, 320, 6, 0);
-	/*
-	input_set_abs_params(idev, ABS_X, 0, 920, 0, 0);
-	input_set_abs_params(idev, ABS_Y, 0, 950, 0, 0);
-	*/
-	input_set_abs_params(idev, ABS_PRESSURE, 0, 256, 0, 0);
-	//input_set_abs_params(idev, ABS_PRESSURE, 0, 1, 0, 0);
+	input_set_abs_params(idev, ABS_PRESSURE, 0, 1, 0, 0);
 
 	error = input_register_device(idev);
 	if (error)
