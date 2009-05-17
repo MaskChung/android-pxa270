dece796575a679da6bb10dbf2d95a733c85103be
mask <mask@bf80cc59-7c04-4460-961f-feaea741d6cd>
2009-05-17 05:27:48 +0000

    update the new building system

--- mkfile/mydroid.mk
+++ mkfile/mydroid.mk
@@ -13,8 +13,7 @@ install_mydroid:
 	-rm -rf $(PRJROOT)/$(call path-for,target-mydroid-rootfs)
 	mkdir -p $(PRJROOT)/$(call path-for,target-mydroid-rootfs)
 ifneq "$(BASE_ROOTFS)" ""
-	file -b $(BASE_ROOTFS) | awk '{print $$1 " -d -c -v $(BASE_ROOTFS)"}' | sh - | tar xvf - --strip-components=1 -C $(PRJROOT)/$(call path-for,target-mydroid-rootfs)
-	rsync -r --exclude='.svn' $(PRJROOT)/$(call path-for,rootfs-overwrite)/* $(PRJROOT)/$(call path-for,target-mydroid-rootfs)
+	cp -af $(PRJROOT)/$(call path-for,target-rootfs) $(PRJROOT)/$(call path-for,target-mydroid-rootfs)
 endif
 	cp -af $(MYDROID_BIN)/system $(PRJROOT)/$(call path-for,target-mydroid-rootfs)
 	cp -af $(MYDROID_BIN)/data $(PRJROOT)/$(call path-for,target-mydroid-rootfs)
@@ -22,6 +21,7 @@ endif
 	cd $(PRJROOT)/$(call path-for,target-mydroid-rootfs)/etc && ln -s ../system/etc/* .
 	rsync -r --exclude='.svn' $(PRJROOT)/$(call path-for,rootfs-overwrite-android)/* $(PRJROOT)/$(call path-for,target-mydroid-rootfs)
 	chmod -R a+rwx $(PRJROOT)/$(call path-for,target-mydroid-rootfs)
+	tar cvzf $(PRJROOT)/$(call path-for,target-bin)/mydroid-rootfs.tgz $(PRJROOT)/$(call path-for,target-mydroid-rootfs)
 
 clean_mydroid:
 	$(MAKE) -C $(PRJROOT)/$(call path-for,mydroid) clean
