2a0a8cd8bcc681da7b1ab293ff03a31788568792
mask <mask@bf80cc59-7c04-4460-961f-feaea741d6cd>
2009-05-17 02:13:21 +0000

    update the new building system

--- mkfile/android-demo.mk
+++ mkfile/android-demo.mk
@@ -9,7 +9,7 @@ install_android_demo:
 ifeq "$(ANDROID_DEMO)" "y"
 	rm -rf $(PRJROOT)/$(call path-for,target-android-demo-rootfs)
 	mkdir -p $(PRJROOT)/$(call path-for,target-android-demo-rootfs)
-	file -b $(DEMO_ROOTFS) | awk '{print $$1 " -d -c -v $(DEMO_ROOTFS)"}' | sh - | tar xvf - -C $(PRJROOT)/$(call path-for,target-android-demo-rootfs)
+	file -b $(DEMO_ROOTFS) | awk '{print $$1 " -d -c -v $(DEMO_ROOTFS)"}' | sh - | tar xvf - --strip-components=1 -C $(PRJROOT)/$(call path-for,target-android-demo-rootfs)
 	chmod -R a+rwx $(PRJROOT)/$(call path-for,target-android-demo-rootfs)
 endif
 
