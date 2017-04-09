0eba8b03ffe27a5d5a45fd8df7db0dcfe8765fbf
Alice Chung <krad1969@gmail.com>
2017-04-09 18:59:31 +0800

    Update xt_dscp.h

--- kernel/linux-2.6.25-android-1.0_r1/include/linux/netfilter/xt_dscp.h
+++ kernel/linux-2.6.25-android-1.0_r1/include/linux/netfilter/xt_dscp.h
@@ -9,7 +9,7 @@
 */
 #ifndef _XT_DSCP_H
 #define _XT_DSCP_H
-
+#include <linux/netfilter/xt_dscp.h>
 #define XT_DSCP_MASK	0xfc	/* 11111100 */
 #define XT_DSCP_SHIFT	2
 #define XT_DSCP_MAX	0x3f	/* 00111111 */
